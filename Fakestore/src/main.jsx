3b0fea2fd379d3af5246aaef9d84f5bde3f5f6e8
Carlos Valeriano <carlos.valeriano@tecsup.edu.pe>
2025-11-05 22:32:00 -0500

    Implementacion del main y app

--- Fakestore/src/main.jsx
+++ Fakestore/src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Products from './pages/Products.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/entities',
+        element: <Products />,
+      },
+    ],
+  },
+])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
